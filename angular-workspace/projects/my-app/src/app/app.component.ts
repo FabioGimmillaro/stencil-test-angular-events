1b5ed33acd5b2f398cde1756a0dbb873198eab55
Fabio Gimmillaro <fabio.gimmillaro@compeople.com>
2023-10-30 12:05:52 +0100

    add angular project

---
+++ angular-workspace/projects/my-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app';
+
+  test(e: CustomEvent<string>) {
+    console.log("TEST", e.detail);
+  }
+}
